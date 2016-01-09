daac07c24c57b321f7a199e022608fe05b5d1a09
enoshcohen <enoshc@gmail.com>
2016-01-09 16:41:06 +0200

    KAKAKAKAKAK

--- Messaging/urls.py
+++ Messaging/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^', include('django.contrib.auth.urls'), name="login"),
-    url(r'^[0-9]+/$', views.messages, name="message")
+    url(r'^login/', include('django.contrib.auth.urls'), name="login"),
+    url(r'^[0-9]+/$', views.messages, name="message"),
 ]
--- Messaging/views.py
+++ Messaging/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import models
+from django.contrib.auth import views as auth_views
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'messaging/index.html', {})
+    cur_usr = request.user
+    return render(request, 'messaging/index.html', {'cur_usr' : cur_usr})
+
 
 
 @login_required
 def messages(request):
     cur_usr = request.user
-    return render(request, 'messaging/messages.html', {'cur_usr' : cur_usr})
+    return render(request, 'messaging/messages.html', {'cur_usr': cur_usr})
 
